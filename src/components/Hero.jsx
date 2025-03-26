7c93002a5bb27e5f999caa5985ccbb28866af055
Ayomide Awofisayo <ayomide_aw@Ayomides-MacBook-Pro-2.local>
2025-03-26 17:34:07 -0400

    nav bar update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='h-max md:h-screen bg-gradient-to-l bg-gray-950 text-white flex flex-col jsutify-center items-center relative z-10 pb-10'>
+      Hero
+    </section>
+  )
+}
+
+export default Hero
