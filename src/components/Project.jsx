6b9a59cdedf7f2a07aef25d83ebc8e6bdf141d0d
Ayomide Awofisayo <ayomide_aw@Ayomides-MacBook-Pro-2.local>
2025-04-28 04:00:14 -0400

    Project Update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import pool from '../assets/pool.png'
 import HT from '../assets/HT.png'
 import TODO from '../assets/TODO.png'
 import calc from '../assets/calc.png'
 import mon from '../assets/mon.png'
+import sign from '../assets/SignLang.png'
+import connect from '../assets/connect4.png'
 import { FaGithub } from 'react-icons/fa'
 
+
 const Project = () => {
+
+    const [expanded, setExpanded] = useState({}) // Track which projects are expanded
+
+    const toggleDescription = (index) => {
+        setExpanded((prev) => ({ ...prev, [index]: !prev[index] }))
+    }
     const projects = [
         // {
         //     title: "H&T Growth Index",
@@ -17,11 +26,30 @@ const Project = () => {
         //     image: HT,
         // },
         {
+            title: "C0NN3CT4",
+            description: "A self-learning Connect Four AI built using Python and reinforcement learning, designed to master gameplay through self-play without any pre-existing data. The system uses Q-learning and deep Q-networks (DQN) to improve over time, tracking win/loss/draw performance. Users can challenge the trained agent through a sleek web interface powered by React and FastAPI, showcasing human-AI interaction, strategic decision-making, and real-time model inference.",
+            techStack: ["Python", "Q-Learning", "Pytorch", "NumPy", "FastAPI", "React", "Vercel", "Matplotlib"],
+            //link: "https://sheltrix.socs.uoguelph.ca",
+            github: 'https://github.com/AyomideAw/Connect4',
+            status: "In Progress",
+            image: connect,
+        },
+        {
+            title: "Sign Language Translator",
+            description: "A real-time sign language translation platform built using TensorFlow and Python to translate sign gestures. Users can use their webcam to detect both static and dynamic signs, with output displayed instantly on the web interface. Designed with accessibility in mind, the tool supports deaf and hard-of-hearing users, educators, and inclusive communication. Emphasis is placed on accurate detection, user-friendly visuals, and seamless integration of AI into everyday interaction.",
+            techStack: ["Python", "TensorFlow Object Detection API", "OpenCV", "Jupyter", "LabelImg"],
+            //link: "https://sheltrix.socs.uoguelph.ca",
+            github: 'https://github.com/AyomideAw/SignLangV1',
+            status: "In Progress",
+            image: sign,
+        },
+        {
             title: "H&T Growth Index",
             description: "A regional data visualization tool built using Python to display real-time housing and employment trends in the Toronto-Hamilton area. Users can explore metrics like unemployment rates, housing starts, and sectoral employment. Focused on clean visuals, reliable datasets, and decision-making support for planners and developers.",
             techStack: ["Python", "JavaScript", "Java", "RESTful API", "TypeScript", "SQL"],
             link: "https://sheltrix.socs.uoguelph.ca",
             github: 'https://github.com/AyomideAw/Metrohousing',
+            status: "Completed",
             image: HT,
         },
         {
@@ -30,6 +58,7 @@ const Project = () => {
             techStack: ["Python", "tkinter"],
             link: "file:///Users/ayomide_aw/Desktop/CIS2750/FinalAssignment/8ball/snooker.html",
             github: 'https://github.com/AyomideAw/To-Do-List',
+            status: "Completed",
             image: TODO,
         },
         {
@@ -38,6 +67,7 @@ const Project = () => {
             techStack: ["React.js", "tkinter", "JavaScript", "CSS"],
             link: "file:///Users/ayomide_aw/Desktop/CIS2750/FinalAssignment/8ball/snooker.html",
             github: 'https://github.com/AyomideAw/calculator',
+            status: "Completed",
             image: calc,
         },
         {
@@ -46,6 +76,7 @@ const Project = () => {
             techStack: ["Figma", "UI/UX"],
             link: "https://www.figma.com/proto/VBAyoESY6nnKQJxT1LXZvO/The-Monitor?node-id=22-11&p=f&t=T0IuTD3xgDmCcQFM-1&scaling=scale-down&content-scaling=fixed&page-id=22%3A10",
             //github: 'https://github.com/AyomideAw/calculator',
+            status: "Completed",
             image: mon,
         },
         // {
@@ -72,17 +103,35 @@ const Project = () => {
             <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {projects.map((project, index) => (
                     <div key={index} className='bg-gray-900 rounded-lg shadow-lg overflow-hidden transform transition-transform hover:scale-105'>
+                        {/* STATUS BADGE */}
+                        {project.status && (
+                            <div className={`absolute top-4 left-4 text-xs font-semibold px-3 py-1 rounded-full z-10
+                                ${project.status === 'Completed' ? 'bg-green-600 text-white' : ''}
+                                ${project.status === 'In Progress' ? 'bg-yellow-500 text-black' : ''}
+                                ${project.status === 'Discontinued' ? 'bg-red-600 text-white' : ''}
+                            `}>
+                            {project.status}
+                            </div>
+                        )}
                         <img src={project.image} alt={project.title} className='w-full h-48 object-cover px-3 pt-3 rounded-2xl'/>
                         <div className='p-6'>
                             <h3 className='text-2xl font-semibold text-gray-300 mb-2'>{project.title}</h3>
-                            <p className='text-gray-300 mb-4'>{project.description}</p>
+                            <p className='text-gray-300 mb-2'>{project.description.length > 250 && !expanded[index] ? project.description.slice(0, 250) + '...': project.description}</p>
+                            {project.description.length > 250 && (
+                                <button onClick={() => toggleDescription(index)}className='text-blue-400 text-sm hover:underline mb-4 -mt-2'>
+                                    {expanded[index] ? 'Show Less' : 'Show More'}
+                                </button>
+                            )}
+
                             <div className='flex flex-wrap gap-2 mb-4'>
                                 {project.techStack.map((tech, idx) => (
                                     <div key={idx} className='bg-gray-950 text-gray-300 text-sm font-medium px-3 py-1 rounded-full'>{tech}</div>
                                 ))}
                             </div>
                             <div className='flex gap-3'>
-                                <a href={project.link} target='_blank' rel='noopener noreferrer' className='inline-block bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-colors'>View Project</a>
+                                {project.link && (
+                                    <a href={project.link} target='_blank' rel='noopener noreferrer' className='inline-block bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-colors'>View Project</a>
+                                )}
                                 {project.github && (
                                     <a href={project.github} target='_blank' rel='noopener noreferrer' className='flex gap-2 items-center bg-gray-950 text-white px-6 py-2 rounded-lg shadow-md hover:bg-gray-800 transition-colors'><FaGithub/>Github</a>
                                 )}
